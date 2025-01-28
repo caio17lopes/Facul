0eb65d850d284bfc0438c7ef1e2847f530be5390
caio17lopes <caio17lopes@gmail.com>
2025-01-28 16:54:19 -0300

    aula2

---
+++ Introdução algoritimos/aula2Algoritimos.py
@@ -0,0 +1,12 @@
+print('hello world!')
+# aspás são para textos
+
+# outra caracteristicas do print como soma etals
+print (2+3) # [e igual a 5]
+
+# concatenar 
+print ('2' + '3')
+print ('Olá ' + 'Mundo!')
+
+# concatenando numeros e frases
+print('O resultado da soma de 2+3 é:', 2+3)
