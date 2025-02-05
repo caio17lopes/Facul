36ee9f493e7f799b30c06281c60e48a0170cd85b
caio17lopes <caio17lopes@gmail.com>
2025-02-04 21:18:22 -0300

    Faculdade

---
+++ Introdução algoritimos/aula2_pratica_b.py
@@ -0,0 +1,12 @@
+'''
+Aula pratica de atribuição 
+    a) Atribuir o valor inteiro 3 à variável
+    b) atribuir o valor 4 à variável
+    c) Atribuir à variavel c o valor da expressão a*a + b*b
+'''
+a = 3
+b = 4
+c = a*a+b*b
+print(c)
+
+# Continuar no minuto 13:38
---
+++ Introdução algoritimos/tabela_operadores.py
@@ -0,0 +1,13 @@
+"""
+Python                        / Operação
+
++           / adição
+-           / subtração
+*           / multiplicação
+/           / divisão               
+//          / divisão somente a parte inteira 
+%           / modulos, resto da divisão 
+**          / exponenciação ou potenciação
+
+
+"""
