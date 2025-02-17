52c29e383b40cb61cb2f113cc62cf294d32aed0c
caio17lopes <caio17lopes@gmail.com>
2025-02-16 21:51:18 -0300

    Atualização

--- Introdução algoritimos/tabela_operadores.py
+++ Introdução algoritimos/tabela_operadores.py
@@ -1,5 +1,6 @@
 """
-Python                        / Operação
+OPERADORES ARITIMÉTICOS
+Python      / Operação
 
 +           / adição
 -           / subtração
@@ -8,6 +9,21 @@ Python                        / Operação
 //          / divisão somente a parte inteira 
 %           / modulos, resto da divisão 
 **          / exponenciação ou potenciação
+
+OPERADORES RELACIONAIS
+
+==          /   igualdade
+>           /   maior que
+<           /   menor que
+>=          /   maior ou igual
+<=          /   menor ou igual
+!=          /   diferente
+
+OPERADORES LOGIOCS
+
+not         /   não     /   negação
+and         /   e       /   conjunção       
+or          /   or      /   disjunção
 
 
 """
