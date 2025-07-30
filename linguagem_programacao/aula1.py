0cdb65ddaecbd18750cad4ca244bb904b93ec4ab
caio17lopes <caio17lopes@gmail.com>
2025-07-29 21:06:46 -0300

    Criação do main
    Signed-off-by: caio17lopes <caio17lopes@gmail.com>

--- linguagem_programacao/aula1.py
+++ linguagem_programacao/aula1.py
@@ -1,5 +1,7 @@
 # 1- Como criar
+
 import numpy
+import time 
 
 # O que é um numpy: é uma biblioteca que oferece estruturas de dados não nativas do python com suporte matemáticas
 # Porque numpy: oferece desempenho e ferramentas superiores à biblioteca padrão
@@ -21,14 +23,14 @@ import numpy
 # end_time = time.time()
 # elapsed_time = end_time - start_time
 # print(f'A criação de lista de 1 bilhão de elementos levou: {elapsed_time}')
-#
+
 # start_time = time.time()
 # ndarray = numpy.zeros(1000000000)
 # end_time = time.time()
 # elapsed_time = end_time - start_time
 # print(f'A criação de um ndarray de 1 bilhão de elementos levou: {elapsed_time}')
 
-# # 3- Dá para fazer ainda melhor se definirmo o tipo de dado, exemplo: dado tipo int8 (para valores de 0 - 255)
+#3- Dá para fazer ainda melhor se definirmo o tipo de dado, exemplo: dado tipo int8 (para valores de 0 - 255)
 # start_time = time.time()
 # ndarray_uint8 = numpy.zeros(1000000000, dtype='uint8')
 # end_time = time.time()
@@ -49,15 +51,13 @@ import numpy
 # matriz = rng.random([4, 4])
 # m_coluna = numpy.sort(matriz, axis=0)
 # m_linha = numpy.sort(matriz, axis=1)
-# m_col_lin = numpy.sort(m_linha, axis=1)
+# m_col_lin = numpy.sort(m_linha, axis=0)
 # print(f'Ordenação dentro coluna:\n{m_coluna}')
 # print(f'Ordenação dentro linha :\n{m_linha}')
 # print(f'Ordenação dentro coluna e linha :\n{m_col_lin}')
 
 # 5 - Agora vamos preparar alguns ndarrays para serem representados por gráficos:
-a = 3 / 4
-b = 2 / 4
-c = 1 / 4
+
 vetor_a = numpy.linspace(10, 1000, 100)
 vetor_b = numpy.linspace(10, 3000, 100)
 vetor_c = numpy.linspace(10, 8000, 100)
@@ -66,12 +66,12 @@ print(vetor_a)
 print(vetor_b)
 print(vetor_c)
 
-# Numpy já tem métodos bem diretos de como salvar um arquivo no formato .txt
+# # # Numpy já tem métodos bem diretos de como salvar um arquivo no formato .txt
 numpy.savetxt('vetor_a.txt', vetor_a, fmt='%f', delimiter=';')
 numpy.savetxt('vetor_b.txt', vetor_b, fmt='%f', delimiter=';')
 numpy.savetxt('vetor_c.txt', vetor_c, fmt='%f', delimiter=';')
 
-# 6 - Agora podemos utilizar uma das várias bibliotecas para plotar gráficos, eu escolhi a plotly
+# # 6 - Agora podemos utilizar uma das várias bibliotecas para plotar gráficos, eu escolhi a plotly
 import plotly.express
 
 array_a = numpy.loadtxt('vetor_a.txt', dtype=numpy.float64, delimiter=';')
