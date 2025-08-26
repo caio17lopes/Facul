0bb82abf2ddad8c789910c501bc4880648b9d9e6
caio17lopes <caio17lopes@gmail.com>
2025-08-26 20:40:08 -0300

    Faculdade

---
+++ aula5/Intro_SQLite3.py
@@ -0,0 +1,7 @@
+'''
+Introdução SQLite3
+
+E um sistema de gerenciamento de banco de dados relacional embutito(RDBMS)
+Conhecido por ser leve, rapido confiavel e não requer configurações de servidores adicionais 
+Ele é uma biblioteca que fornece uma maneira de armazenar, gerenciar e recuperar dados estruturados usando linguagem SQL 
+'''
